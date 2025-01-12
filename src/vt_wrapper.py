1eda6c0197119c19d8bb482d7bdbaec86db4257c
Nixam01 <marlatawiec01@gmail.com>
2025-01-12 21:28:56 +0100

    back to work on wrapper, testing basic json parsing capabilities

--- src/vt_wrapper.py
+++ src/vt_wrapper.py
@@ -4,22 +4,43 @@ import json
 # Replace 'your-api-key' with your actual VirusTotal API key
 with open('api.key', 'r') as file:
     API_KEY = file.read()
-DOMAIN = "example.com"  # Replace with the domain you want to check
 
-# Set the API endpoint and headers
-url = f"https://www.virustotal.com/api/v3/domains/{DOMAIN}"
 headers = {
     "accept": "application/json",
     "x-apikey": API_KEY
 }
+def check_domain(domain):
+    try:
+        url = f"https://www.virustotal.com/api/v3/domains/{domain}"
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        data = response.json()  # Parse the JSON response
 
-# Perform the GET request
-try:
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()  # Raise an exception for HTTP errors
-    data = response.json()  # Parse the JSON response
+        # Print the JSON response in a pretty format
+        print("Full Response:")
+        print(json.dumps(data, indent=4))
 
-    # Print the JSON response in a pretty format
-    print(json.dumps(data, indent=4))
-except requests.exceptions.RequestException as e:
-    print(f"An error occurred: {e}")
+        # Extract and print last_analysis_stats
+        last_analysis_stats = data.get("data", {}).get("attributes", {}).get("last_analysis_stats", {})
+        print("\nLast Analysis Stats:")
+        print(json.dumps(last_analysis_stats, indent=4))
+
+        malicious_stats = data.get("data", {}).get("attributes", {}).get("last_analysis_stats", {}).get("malicious", {})
+        print("\nMalicious Stats:")
+        print(json.dumps(malicious_stats, indent=4))
+
+        if (malicious_stats > 4):
+            print(f"Domain {domain} is malicious")
+        else:
+            print(f"Domain {domain} is not malicious")
+
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+def main():
+
+    domain = input("Enter the domain you want to check: ")
+    check_domain(domain)
+
+if __name__ == "__main__":
+    main()
