93398ac434a871e6a3a15f29f99bedd59c3b5ead
jromanek3 <01159072@pw.edu.pl>
2025-02-18 19:25:10 +0100

    Added logic to extract features from a domain

--- src/vt_wrapper.py
+++ src/vt_wrapper.py
@@ -1,9 +1,13 @@
 import requests
 import json
+from datetime import datetime
+
 
 # Replace 'your-api-key' with your actual VirusTotal API key
-with open('api.key', 'r') as file:
-    API_KEY = file.read()
+# with open('api.key', 'r') as file:
+#     API_KEY = file.read()
+
+API_KEY = "6ad968cbce66bc137d7f2dc51edfbd87834aad5839ab6263ef4a814d6968acf4"
 
 headers = {
     "accept": "application/json",
@@ -17,26 +21,37 @@ def check_domain(domain):
         data = response.json()  # Parse the JSON response
 
         # Print the JSON response in a pretty format
-        print("Full Response:")
-        print(json.dumps(data, indent=4))
+        #print("Full Response:")
+        #print(json.dumps(data, indent=4))
 
         # Extract and print last_analysis_stats
-        last_analysis_stats = data.get("data", {}).get("attributes", {}).get("last_analysis_stats", {})
-        print("\nLast Analysis Stats:")
-        print(json.dumps(last_analysis_stats, indent=4))
+        #last_analysis_stats = data.get("data", {}).get("attributes", {}).get("last_analysis_stats", {})
+        #print("\nLast Analysis Stats:")
+        #print(json.dumps(last_analysis_stats, indent=4))
 
-        malicious_stats = data.get("data", {}).get("attributes", {}).get("last_analysis_stats", {}).get("malicious", {})
-        print("\nMalicious Stats:")
-        print(json.dumps(malicious_stats, indent=4))
+        #malicious_stats = data.get("data", {}).get("attributes", {}).get("last_analysis_stats", {}).get("malicious", {})
+        #print("\nMalicious Stats:")
+        #print(json.dumps(malicious_stats, indent=4))
 
-        if (malicious_stats > 4):
-            print(f"Domain {domain} is malicious")
-        else:
-            print(f"Domain {domain} is not malicious")
+        #if (malicious_stats > 4):
+        #    print(f"Domain {domain} is malicious")
+        #else:
+        #    print(f"Domain {domain} is not malicious")
+
+        domain_creation_date = data.get("data", {}).get("attributes", {}).get("creation_date")
+        malicious_hits = data.get("data", {}).get("attributes", {}).get("total_votes", {}).get("malicious")
+        now = datetime.now()
+        not_valid_before = datetime.strptime(data.get("data", {}).get("attributes", {}).get("last_https_certificate", {}).get("validity", {}).get("not_before"), "%Y-%m-%d %H:%M:%S")
+        not_valid_after = datetime.strptime(data.get("data", {}).get("attributes", {}).get("last_https_certificate", {}).get("validity", {}).get("not_after"), "%Y-%m-%d %H:%M:%S")
+        is_certificate_valid = not_valid_before <= now <= not_valid_after
+
+
+        return domain_creation_date, malicious_hits, is_certificate_valid
 
 
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
+        return None, None, None
 def main():
 
     domain = input("Enter the domain you want to check: ")
